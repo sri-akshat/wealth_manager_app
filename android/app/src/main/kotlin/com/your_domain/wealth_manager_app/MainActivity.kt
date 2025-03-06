8f6cc92d4431da11924cc6d5341418cd0bacdf8d
sri-akshat <sri.akshat@gmail.com>
2025-03-07 05:25:08 +0530

    Fix Android embedding and update CI workflow

---
+++ android/app/src/main/kotlin/com/your_domain/wealth_manager_app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.your_domain.wealth_manager_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // No need for additional code - this is the V2 embedding approach
+} 
